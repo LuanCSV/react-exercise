e086fb539bd5ea6fd24b20d8ee1a5955c2141efd
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-10-13 20:36:06 -0300

    feature: toggle theme

--- project/src/components/Header/index.js
+++ project/src/components/Header/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../logo.svg';
 import './style.css';
 
+// useState consiste em um estado inicial, 
+// depois numa função com um método q vai definir a mudança desse estado
+
 const Header = () => {
-    const temaAtual = 'light'
+    const [tema, setTema] = useState('dark') ;
+
+    const toggleTema = () => {
+        let novoTema = tema === 'dark' ? 'light' : 'dark';
+        setTema(novoTema);
+    }
 
     return (
-        <header className="App-header">
+        <header 
+        className={`App-header App-header--${tema}`}
+        onClick={() => toggleTema()}
+        >
             <img 
                 src={logo}
                 className="App-logo"
                 alt="Logo"
             /> 
-            <h1> {temaAtual} </h1>
+            <h1> {tema} </h1>
             <a
-                className="App-link"
+                className= {`App-link App-link--${tema}`}
                 href="https://github.com/LuanCSV/react-exercise"
                 target="_blank"
                 rel="noopener noreferrer"
