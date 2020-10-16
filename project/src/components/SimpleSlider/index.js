e7628c716d6a6edf42ed53e6f2d272860f807198
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-10-15 21:58:05 -0300

    bugfix: dots appearing again

--- project/src/components/SimpleSlider/index.js
+++ project/src/components/SimpleSlider/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Slider from 'react-slick';
 import slides from './slides.json';
 import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick-theme.css";
 import './styles.css';
 
 
