c040c9085ae08e3f6b9b7c9c82a83e4586c69c03
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-10-15 21:39:26 -0300

    feature: slides from json

--- project/src/components/SimpleSlider/index.js
+++ project/src/components/SimpleSlider/index.js
@@ -1,43 +1,48 @@
 import React, { Component } from 'react';
 import Slider from 'react-slick';
+import slides from './slides.json';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
+console.log(slides);
 
 export default class SimpleSlider extends Component {
     render() {
         const settings = {
+            accessibility: true,
+            adaptativeHeight: false,
             arrows: false,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            className: 'simple-slider-slide',
             dots: true,
+            draggable: true,
+            easing: 'ease-in-out',
             infitine: true,
             speed: 500, 
             slidesToShow: 1,
-            slidersToScroll: 1
+            slidersToScroll: 1,
+            swipeToSlider: true
         };
     
         return (
-            <div>
-                <h2>Single Item</h2>
+            <div className="mega-ultra-slider">
+                <h2>Mega ultra slider</h2>
                 <Slider {...settings}>
-                    <div>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h3>4</h3>
-                    </div>
-                    <div>
-                        <h3>5</h3>
-                    </div>
-                    <div>
-                        <h3>6</h3>
-                    </div>
+                    {
+                        slides.map((item, index) => {
+                            return (
+                                <img
+                                    key={index}
+                                    src={item.imgSrc}
+                                    alt={`Esse e o slide ${item.id+1}`}
+                                    height="auto"
+                                    width="100%"
+                                />
+                            )
+                        })
+                    }
                 </Slider>
             </div>
         )
