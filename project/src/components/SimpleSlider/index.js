a82d382e15d94e29b6f984f74049031ebb9308e8
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-10-15 21:53:24 -0300

    feature: layout do slider

--- project/src/components/SimpleSlider/index.js
+++ project/src/components/SimpleSlider/index.js
@@ -3,6 +3,7 @@ import Slider from 'react-slick';
 import slides from './slides.json';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import './styles.css';
 
 
 console.log(slides);
